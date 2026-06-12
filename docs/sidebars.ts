d197c628e0c7040fe2e500a54f54ab6d0e293e46
andrewfulton9 <andrewfulton9@gmail.com>
2026-06-12 11:13:16 -0600

    update sidebar

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -3,33 +3,6 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   docsSidebar: [
     'introduction',
-    {
-      type: 'category',
-      label: 'Deployment',
-      link: {type: 'doc', id: 'deployment/index'},
-      items: [
-        'deployment/deploy',
-        'deployment/architecture',
-        'deployment/values',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'User Guide',
-      link: {type: 'doc', id: 'user-guide/index'},
-      items: [
-        'user-guide/use',
-        'user-guide/troubleshoot',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Reference',
-      link: {type: 'doc', id: 'reference/index'},
-      items: [
-        'reference/release-notes',
-      ],
-    },
   ],
 };
 
